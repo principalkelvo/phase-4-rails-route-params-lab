a9b843190c3db8a1c5ed1d22d909cd8b1752aabd
principalkelvo <principalkelvo@gmail.com>
2022-09-27 19:05:51 +0300

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # if params[:name]
+    # students = Student.find_by(first_name: params[:name]|| last_name: params[:name])
+    # else 
+      students = Student.all
+    # end
+    render json: students
+  end
+
+  def show
+    students = Student.find(params[:id])
     render json: students
   end
 
